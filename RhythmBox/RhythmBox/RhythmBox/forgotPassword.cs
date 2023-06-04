285f6c6a5e7f4aa1570a5c96ff006ff89652f28c
Huan <nvhuan2907@gmail.com>
2023-06-04 21:09:02 +0700

    forgot password form

---
+++ RhythmBox/RhythmBox/RhythmBox/forgotPassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RhythmBox
+{
+    public partial class forgotPassword : Form
+    {
+        public forgotPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            new enterCodeResetPassword().Show();
+            this.Hide();
+        }
+    }
+}
