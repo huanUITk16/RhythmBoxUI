542fcdc7dff40e8beb211c2d0fb26a14f4bef5b6
longpc5 <longnuyn392@gmail.com>
2023-06-06 22:23:24 +0700

    Add TokenManager

---
+++ RhythmBox/RhythmBox/RhythmBox/TokenManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhythmBox
+{
+    internal static class TokenManager
+    {
+        private static string _accessToken;
+
+        public static void SetAccessToken(string accessToken)
+        {
+            if (_accessToken == null)
+                _accessToken = accessToken;
+        }
+
+        public static string GetAccessToken() { return _accessToken; }
+    }
+}
