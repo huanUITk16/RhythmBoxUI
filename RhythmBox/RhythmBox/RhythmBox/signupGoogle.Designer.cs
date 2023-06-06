0f2ef21b07ac35be0704a0fff165bbff22975a2b
Huan <nvhuan2907@gmail.com>
2023-06-06 11:12:33 +0700

    sign up with google form

---
+++ RhythmBox/RhythmBox/RhythmBox/signupGoogle.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace RhythmBox
+{
+    partial class signupGoogle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox_no = new System.Windows.Forms.CheckBox();
+            this.checkBox_female = new System.Windows.Forms.CheckBox();
+            this.checkBox_male = new System.Windows.Forms.CheckBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_year = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cbBox_month = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_day = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_signup = new FontAwesome.Sharp.IconButton();
+            this.checkbox_show = new System.Windows.Forms.CheckBox();
+            this.txt_confirmSUG = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_passwordSUG = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox_no
+            // 
+            this.checkBox_no.AutoSize = true;
+            this.checkBox_no.Location = new System.Drawing.Point(27, 533);
+            this.checkBox_no.Name = "checkBox_no";
+            this.checkBox_no.Size = new System.Drawing.Size(189, 24);
+            this.checkBox_no.TabIndex = 60;
+            this.checkBox_no.Text = "Don\'t prefer to say";
+            this.checkBox_no.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_female
+            // 
+            this.checkBox_female.AutoSize = true;
+            this.checkBox_female.Location = new System.Drawing.Point(155, 503);
+            this.checkBox_female.Name = "checkBox_female";
+            this.checkBox_female.Size = new System.Drawing.Size(92, 24);
+            this.checkBox_female.TabIndex = 59;
+            this.checkBox_female.Text = "Female";
+            this.checkBox_female.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_male
+            // 
+            this.checkBox_male.AutoSize = true;
+            this.checkBox_male.Location = new System.Drawing.Point(27, 503);
+            this.checkBox_male.Name = "checkBox_male";
+            this.checkBox_male.Size = new System.Drawing.Size(71, 24);
+            this.checkBox_male.TabIndex = 58;
+            this.checkBox_male.Text = "Male";
+            this.checkBox_male.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label11.Location = new System.Drawing.Point(23, 480);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 20);
+            this.label11.TabIndex = 57;
+            this.label11.Text = "Gender:";
+            // 
+            // txt_year
+            // 
+            this.txt_year.Location = new System.Drawing.Point(268, 435);
+            this.txt_year.Name = "txt_year";
+            this.txt_year.Size = new System.Drawing.Size(90, 27);
+            this.txt_year.TabIndex = 56;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label10.Location = new System.Drawing.Point(264, 411);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 20);
+            this.label10.TabIndex = 55;
+            this.label10.Text = "Year";
+            // 
+            // cbBox_month
+            // 
+            this.cbBox_month.FormattingEnabled = true;
+            this.cbBox_month.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cbBox_month.Location = new System.Drawing.Point(115, 434);
+            this.cbBox_month.Name = "cbBox_month";
+            this.cbBox_month.Size = new System.Drawing.Size(121, 28);
+            this.cbBox_month.TabIndex = 54;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label9.Location = new System.Drawing.Point(111, 411);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 20);
+            this.label9.TabIndex = 53;
+            this.label9.Text = "Month:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label8.Location = new System.Drawing.Point(23, 381);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(233, 20);
+            this.label8.TabIndex = 52;
+            this.label8.Text = "What is your date of birth?";
+            // 
+            // txt_day
+            // 
+            this.txt_day.Location = new System.Drawing.Point(27, 434);
+            this.txt_day.Name = "txt_day";
+            this.txt_day.Size = new System.Drawing.Size(58, 27);
+            this.txt_day.TabIndex = 51;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label7.Location = new System.Drawing.Point(23, 411);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 20);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Day:";
+            // 
+            // txt_email
+            // 
+            this.txt_email.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txt_email.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_email.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_email.Location = new System.Drawing.Point(27, 328);
+            this.txt_email.Multiline = true;
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(303, 29);
+            this.txt_email.TabIndex = 49;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label6.Location = new System.Drawing.Point(23, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Email:";
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.BackColor = System.Drawing.SystemColors.InfoText;
+            this.btn_signup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_signup.FlatAppearance.BorderSize = 0;
+            this.btn_signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_signup.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_signup.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_signup.IconChar = FontAwesome.Sharp.IconChar.SignOutAlt;
+            this.btn_signup.IconColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_signup.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.btn_signup.IconSize = 30;
+            this.btn_signup.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_signup.Location = new System.Drawing.Point(82, 597);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Padding = new System.Windows.Forms.Padding(40, 0, 20, 0);
+            this.btn_signup.Size = new System.Drawing.Size(200, 44);
+            this.btn_signup.TabIndex = 47;
+            this.btn_signup.Text = "Sign Up";
+            this.btn_signup.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_signup.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_signup.UseVisualStyleBackColor = false;
+            // 
+            // checkbox_show
+            // 
+            this.checkbox_show.AutoSize = true;
+            this.checkbox_show.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkbox_show.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.checkbox_show.Location = new System.Drawing.Point(200, 288);
+            this.checkbox_show.Name = "checkbox_show";
+            this.checkbox_show.Size = new System.Drawing.Size(160, 24);
+            this.checkbox_show.TabIndex = 46;
+            this.checkbox_show.Text = "Show Password";
+            this.checkbox_show.UseVisualStyleBackColor = true;
+            // 
+            // txt_confirmSUG
+            // 
+            this.txt_confirmSUG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txt_confirmSUG.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_confirmSUG.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_confirmSUG.Location = new System.Drawing.Point(27, 253);
+            this.txt_confirmSUG.Multiline = true;
+            this.txt_confirmSUG.Name = "txt_confirmSUG";
+            this.txt_confirmSUG.PasswordChar = '*';
+            this.txt_confirmSUG.Size = new System.Drawing.Size(303, 29);
+            this.txt_confirmSUG.TabIndex = 45;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(23, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 20);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Confirm Password:";
+            // 
+            // txt_passwordSUG
+            // 
+            this.txt_passwordSUG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txt_passwordSUG.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_passwordSUG.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_passwordSUG.Location = new System.Drawing.Point(27, 183);
+            this.txt_passwordSUG.Multiline = true;
+            this.txt_passwordSUG.Name = "txt_passwordSUG";
+            this.txt_passwordSUG.PasswordChar = '*';
+            this.txt_passwordSUG.Size = new System.Drawing.Size(303, 29);
+            this.txt_passwordSUG.TabIndex = 43;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label3.Location = new System.Drawing.Point(23, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Password:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(233)))));
+            this.txt_name.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_name.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_name.Location = new System.Drawing.Point(27, 114);
+            this.txt_name.Multiline = true;
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(303, 29);
+            this.txt_name.TabIndex = 41;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label2.Location = new System.Drawing.Point(23, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 20);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "What should I call you?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 25);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Sign Up with Google";
+            // 
+            // signupGoogle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(373, 783);
+            this.Controls.Add(this.checkBox_no);
+            this.Controls.Add(this.checkBox_female);
+            this.Controls.Add(this.checkBox_male);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_year);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cbBox_month);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_day);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_signup);
+            this.Controls.Add(this.checkbox_show);
+            this.Controls.Add(this.txt_confirmSUG);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_passwordSUG);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "signupGoogle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "signupGoogle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox_no;
+        private System.Windows.Forms.CheckBox checkBox_female;
+        private System.Windows.Forms.CheckBox checkBox_male;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_year;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox cbBox_month;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_day;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label6;
+        private FontAwesome.Sharp.IconButton btn_signup;
+        private System.Windows.Forms.CheckBox checkbox_show;
+        private System.Windows.Forms.TextBox txt_confirmSUG;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_passwordSUG;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
